5950e4954fa5ed67f88a08f7e59bd1706179b8fb
Kenan Kadic <kkadic@midtownhi.com>
2022-02-18 10:30:49 -0600

    portfolio links updated

--- components/portfolio.vue
+++ components/portfolio.vue
@@ -5,9 +5,9 @@
       <h1> {{ name }}</h1>
       <p> {{ description }}</p>
       <div class="d-flex">
-        <v-icon class="icon mx-2">mdi-github</v-icon>
-        <v-icon class="icon mx-2">mdi-linkedin</v-icon>
-        <v-icon class="icon mx-2">mdi-facebook</v-icon>
+        <a href="https://www.github.com" target="_blank" title="Github"><v-icon class="icon mx-2">mdi-github</v-icon></a>
+        <a href="https://www.linkedin.com" target="_blank" title="LinkedIn"><v-icon class="icon mx-2">mdi-linkedin</v-icon></a>
+        <a href="https://www.facebook.com" target="_blank" title="Facebook"><v-icon class="icon mx-2">mdi-facebook</v-icon></a>
       </div>
     </div>
   </v-container>
@@ -38,6 +38,10 @@ export default {
     font-size: 44px;
     color: yellowgreen;
   }
+
+    a{
+      text-decoration: none;
+    }
 }
 
 
